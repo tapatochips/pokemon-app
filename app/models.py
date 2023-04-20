3f7e16582588ac8d5a99f56c848c7638305973a9
Daniel Richmond <danielrichmond@Daniels-MacBook-Pro.local>
2023-04-19 19:42:25 -0500

    trying to add catch feature and store pokemon in db

--- app/models.py
+++ app/models.py
@@ -15,7 +15,6 @@ user_pokemon = db.Table("user_pokemon",
 )
 
 
-
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     email = db.Column(db.String(100), nullable = False, unique = True)
@@ -24,7 +23,7 @@ class User(db.Model, UserMixin):
     date_created = db.Column(db.DateTime, nullable = False, default=datetime.utcnow())
     bio = db.Column(db.String, nullable = True)
     img_url = db.Column(db.String, nullable = True)
-    pokemons = db.relationship('Pokemon', secondary=user_pokemon, backref=db.backref('users', lazy='True'))
+    pokemon = db.relationship('Pokemon', secondary=user_pokemon, backref=db.backref('users', lazy='select'))
 
     
     
@@ -52,22 +51,70 @@ class User(db.Model, UserMixin):
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
- 
- 
-    class MyPokemon(db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
-        pokmemon_id = db.Column(db.Integer, db.ForeignKey('pokemon.id'), nullable = False)
+        
+class Pokemon(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    hp = db.Column(db.Integer, nullable=False)
+    attack = db.Column(db.Integer, nullable=False)
+    defense = db.Column(db.Integer, nullable=False)
+    type = db.Column(db.String(50), nullable=False)
+    img_url = db.Column(db.String(300), nullable=False)
 
-        def __init__(self, user_id, pokmemon_id):
-            self.user_id = user_id
-            self.pokmemon_id = pokmemon_id
-        def saveToDB(self):
-            db.session.add(self)
-            db.session.commit()
-        def deleteFromDB(self):
-            db.session.delete(self)
-            db.session.commit()
+    def __init__(self, user_id, pokemon_id, name, hp, attack, defense, type, img_url, api_url):
+        self.user_id = user_id
+        self.pokemon_id = pokemon_id
+        self.name = name
+        self.hp = hp
+        self.attack = attack
+        self.defense = defense
+        self.type = type
+        self.img_url = img_url
+        #thought maybe i need to call the api_url but still didnt work
+        self.api_url = api_url
+    
+    def from_dict(self, data):
+        self.user_id = data['user_id']
+        self.pokemon_id = data['pokemon_id']
+        self.name = data['name']
+        self.hp = data['hp']
+        self.attack = data['attack']
+        self.defense = data['defense']
+        self.type = data['type']
+        self.img_url = data['img_url']
+    
+    
+    
+    def saveToDB(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def deleteFromDB(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+
+    
+class MyPokemon(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    pokemon_id = db.Column(db.Integer, db.ForeignKey('pokemon.id'), nullable=False)
+    pokemon_obj = db.relationship('Pokemon', backref=db.backref('my_pokemon_list', lazy=True))
+
+    def __init__(self, user_id, pokemon_id):
+        self.user_id = user_id
+        self.pokemon_id = pokemon_id
+    
+    def saveToDB(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def deleteFromDB(self):
+        db.session.delete(self)
+        db.session.commit()
+
             
+
 
 
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,12 @@
-from flask import Flask, flash, redirect, render_template, request, url_for
+from flask import Flask, flash, redirect, render_template, request, url_for, session
 from app import app
 from .forms import PokemonForm
 import requests
 from flask_login import current_user
 from .models import db, MyPokemon, Pokemon
 
+# Set the secret key for session management
+app.secret_key = 'my_secret_key'
 
 @app.route('/', methods=['GET'])
 def home():
@@ -20,6 +22,7 @@ def pokemon_form():
         and then displaying it in the card for the user
     """
     form = PokemonForm()
+    pokemon = session.get('pokemon_data', None)  #creates pokemon
     if request.method == 'POST':
         if form.validate():
             pokemon_name = form.pokemon_name.data.lower()
@@ -29,55 +32,91 @@ def pokemon_form():
             if response.status_code == 200:
                 data = response.json()
                 pokemon_data = {
+                    # 'stats': data['stats'],
                     'name': data['name'],
                     'hp': data['stats'][0]['base_stat'],
-                    'defense': data['stats'][1]['base_stat'],
                     'attack': data['stats'][2]['base_stat'],
+                    'defense': data['stats'][1]['base_stat'],
+                    'image': data['sprites']['front_shiny'],
+                    'ability': data['abilities'][0]['ability']['name'],
+                    'types': ", ".join([t['type']['name'] for t in data['types']]),
+                }
+                session['pokemon_data'] = pokemon_data
+                pokemon = pokemon_data  #updates pokemon
+                if 'catch' in request.form:
+                    return redirect(url_for('my_pokemon'))  #redirects if user clicked on catch
+            else:
+                flash('Pokemon not found!')
+
+    return render_template('pokemon_form.html', form=form, pokemon=pokemon)
+
+
+
+@app.route('/my_pokemon', methods=['GET', 'POST'])
+def my_pokemon():
+    user_id = current_user.id
+
+    #gets caught pokemon details
+    caught_pokemon = db.session.query(MyPokemon).filter_by(user_id=user_id).all()
+
+    #gets newly caught pokemon details
+    pokemon_data = session.get('pokemon_data')
+    if pokemon_data:
+        try:
+            pokemon = Pokemon(
+                # stats=pokemon_data['stats'],
+                name=pokemon_data['name'],
+                hp=pokemon_data['stats'][0]['base_stat'],
+                attack=pokemon_data['stats'][1]['base_stat'],
+                defense=pokemon_data['stats'][2]['base_stat'],
+                image=pokemon_data['sprites']['front_shiny'],
+                ability=pokemon_data['abilities'][0]['ability']['name'],
+                types=", ".join([t['type']['name'] for t in pokemon_data['types']])
+            )
+        except KeyError as e:
+            flash(f"Error: Missing key '{e.args[0]}' in pokemon_data dictionary.")
+            return redirect(url_for('pokemon_form'))
+
+        db.session.add(pokemon)
+        db.session.commit()
+        session.pop('pokemon_data', None)
+        caught_pokemon.append(pokemon)
+    else:
+        pokemon = None
+
+    return render_template('my_pokemon.html', pokemon_list=caught_pokemon, pokemon=pokemon)
+
+
+@app.route('/catch_pokemon', methods=['GET'])
+def catch_pokemon():
+    pokemon_data = session.get('pokemon_data')
+    if pokemon_data:
+        flash('You have already caught a pokemon!')
+        return redirect(url_for('my_pokemon'))
+    else:
+        #gets newly caught pokemon from request
+        pokemon_name = request.args.get('pokemon_name', None)
+        if pokemon_name:
+            #ask api for pokemon data
+            url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}"
+            response = requests.get(url)
+            if response.status_code == 200:
+                data = response.json()
+                pokemon_data = {
+                    # 'stats': data['stats'],
+                    'name': data['name'],
+                    'hp': data['stats'][0]['base_stat'],
+                    'attack': data['stats'][2]['base_stat'],
+                    'defense': data['stats'][1]['base_stat'],
                     'image': data['sprites']['front_shiny'],
                     'ability': data['abilities'][0]['ability']['name'],
                     'types': ", ".join([t['type']['name'] for t in data['types']])
-
                 }
-                return render_template('pokemon_form.html', pokemon=pokemon_data, form=form)
+                session['pokemon_data'] = pokemon_data
+                return redirect(url_for('my_pokemon', pokemon_data=pokemon_data))
+            else:
+                flash('Pokemon not found!')
         else:
-             flash('Pokemon not found!')
-        return render_template('pokemon_form.html', form=form)
-    return render_template('pokemon_form.html', form=form)
+            flash('No pokemon data found!')
+        return redirect(url_for('pokemon_form'))
 
-
-
-
-@app.route('/my_pokemon', methods = ['GET', 'POST'])
-def my_pokemon():
-    # get the ID of the current user
-    user_id = current_user.id
-
-    # retrieve a list of caught pokemon for the current user
-    caught_pokemon = db.session.query(MyPokemon).filter_by(user_id=user_id).all()
-
-    # get details of each caught Pokemon
-    pokemon_list = []
-    for caught in caught_pokemon:
-        pokemon = db.session.query(Pokemon).filter_by(id=caught.pokemon_id).first()
-        pokemon_list.append(pokemon)
-
-    # render the list of caught Pokemon in a template
-    return render_template('my_pokemon.html', pokemon_list=pokemon_list)
-
-#make button to catch and release and save/ delete respectively.
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
